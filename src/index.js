35af518277fdee52667969588dc568288d4d22da
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-27 18:05:40 -0400

    commit tree 8adef948efd5135db4b9709253f87309045ded22

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
+
+
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white'
+}
+
+/* Add the NavBar Component */
+const NavBar = () => 
+  <div>
+    <NavLink
+      to='/'
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact 
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{background: 'darkblue'}}
+    >Home</NavLink>
+    <NavLink 
+      to='/about'
+      exact
+      style={link}
+      activeStyle={{background: 'darkblue'}}
+      >About</NavLink>
+    <NavLink 
+      to='/login'
+      exact
+      style={link}
+      activeStyle={{background: 'darkblue'}}
+      >Login</NavLink>
+  </div>
+
 
 const Home = () => {
   return (
@@ -9,7 +48,39 @@ const Home = () => {
   );
 };
 
+const About = () => {
+  return (
+    <div>
+      <h1>This is about my component!</h1>
+    </div>
+  )
+}
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type='text' name='username' placeholder='Username' />
+          <label htmlFor='username'>Username</label>
+        </div>
+        <div>
+          <input type='password' name='password' placeholder='Password'/>
+          <label htmlFor='password'>Password</label>
+        </div>
+        <input type='submit' value='Login'/>
+      </form>
+    </div>
+  )
+}
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <NavBar/>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/about' component={About}/>
+      <Route exact path='/login' component={Login} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
